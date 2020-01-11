0522f21ab1a9c519bbc2024e44672e191e99f39a
ZachBaird <zachbairddev@gmail.com>
2020-01-11 11:42:12 -0500

    Reformatting code and adding documentation.

--- main.rb
+++ main.rb
@@ -1,21 +1,28 @@
-def stock_picker(stock_prices)
-  lowest = 9999999999
-  highest = 0
+# Dev: Zach Baird
+# Objective:
+# This method takes in an array of stock prices, where position 0 is day 1, position 1 is day 2, etc.
+#   The method identifies the highest stock value, purchases the lowest stock value that occurs before
+#   the stock hits that highest value, and then sells at the highest for a profit.
 
-  index_of_highest = 0
-  index_of_lowest = 0
+def stock_picker(stock_prices)
+  lowest, highest = 9999999999, 0
+  index_of_highest, index_of_lowest = 0
 
   stock_prices.each_with_index do |price, index|
+    
     if price > highest
       highest, index_of_highest = price, index
     end
+
   end
 
   index = 0
   while index < index_of_highest
+
     if stock_prices[index] < lowest
       lowest, index_of_lowest = stock_prices[index], index
     end
+
     index += 1
   end
   
